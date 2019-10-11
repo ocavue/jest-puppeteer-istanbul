dcabaedc4822ddd6ff5edab6334c6e67ccf32c16
ocavue <ocavue@gmail.com>
2019-10-12 07:15:16 +0800

    tests

---
+++ .circleci/check-example-result.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const fs = require("fs")
+
+function check (path) {
+    const raw = fs.readFileSync(path, "utf-8")
+    console.log(raw)
+    const result = JSON.parse(raw)
+
+    if (result.total.lines.pct !== 100) {
+        throw Error(`Wrong result`)
+    } else {
+        console.log("ok")
+    }
+}
+
+check('example/coverage_result/coverage-summary.json')
--- example/jest.config.js
+++ example/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ["src/**/*"],
-    coverageReporters: ["text", "lcov", "json"],
+    coverageReporters: ["text", "lcov", "json", "json-summary"],
     preset: "jest-puppeteer",
     setupFilesAfterEnv: ["jest-puppeteer-istanbul/lib/setup"],
     reporters: ["default", "jest-puppeteer-istanbul/lib/reporter"],
