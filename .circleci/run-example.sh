397cd23b4d907357af0507ea8a517ca9a6041942
ocavue <ocavue@gmail.com>
2019-10-12 07:38:22 +0800

    update

--- .circleci/run-example.sh
+++ .circleci/run-example.sh
@@ -7,6 +7,6 @@ cd $1
 yarn install
 cp -f /tmp/lib/* ./node_modules/jest-puppeteer-istanbul/lib/
 yarn dev &
-sleep 15
+sleep 5
 yarn test
 JSON_PATH='./coverage_result/coverage-summary.json' root/.circleci/check-example-result.js
