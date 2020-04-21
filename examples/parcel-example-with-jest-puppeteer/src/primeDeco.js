821f7bd140fc07dcaac08b7eb2d0bb37b51d9a9d
ocavue <ocavue@gmail.com>
2020-04-21 22:52:51 +0800

    lint

--- examples/parcel-example-with-jest-puppeteer/src/primeDeco.js
+++ examples/parcel-example-with-jest-puppeteer/src/primeDeco.js
@@ -1,6 +1,6 @@
 // This function will be covered under a unit test (normal jest test)
 module.exports = function primeDeco(n) {
-    result = []
+    const result = []
     for (let i = 2; i <= n; i++) {
         while (n !== i) {
             if (n % i !== 0) {
