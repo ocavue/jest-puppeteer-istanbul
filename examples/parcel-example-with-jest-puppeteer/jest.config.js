1d6ba12d7e489c72d489c772de11619d49371227
ocavue <ocavue@gmail.com>
2021-10-17 16:19:08 +0800

    chore: test coverageThreshold

--- examples/parcel-example-with-jest-puppeteer/jest.config.js
+++ examples/parcel-example-with-jest-puppeteer/jest.config.js
@@ -6,4 +6,12 @@ module.exports = {
     setupFilesAfterEnv: ["jest-puppeteer-istanbul/lib/setup"],
     reporters: ["default", "jest-puppeteer-istanbul/lib/reporter"],
     coverageDirectory: "coverage_result",
+    // coverageThreshold: {
+    //     global: {
+    //         branches: 80,
+    //         functions: 80,
+    //         lines: 80,
+    //         statements: 80,
+    //     },
+    // },
 }
